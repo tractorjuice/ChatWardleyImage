18e7f98d23f2a145d434b9afa06de7c79c3eccab
tractorjuice <129532814+tractorjuice@users.noreply.github.com>
2024-06-18 11:19:38 +0000

    Add spinner

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,8 @@ def handle_file_upload(uploaded_file):
             files = {'file': uploaded_file}
             bearer_token = st.secrets["BEARER_TOKEN"]
             headers = {'Authorization': f'Bearer {bearer_token}'}
-            response = requests.post('https://api.wardleymaps.ai/v1/analyse-wardleymap-image', files=files, headers=headers, timeout=240)
+            with st.spinner("Analysing your Wardley Map ..."):
+                response = requests.post('https://api.wardleymaps.ai/v1/analyse-wardleymap-image', files=files, headers=headers)
 
             if response.status_code == 200:
                 return response.json()

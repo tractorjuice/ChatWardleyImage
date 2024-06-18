dc983fdf3b3f52b195129fa02edb42e92f59cf74
tractorjuice <129532814+tractorjuice@users.noreply.github.com>
2024-06-18 11:17:50 +0000

    Add additional error handling

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,21 +34,23 @@ st.html(custom_css_styling)
 def handle_file_upload(uploaded_file):
     try:
         if uploaded_file:
-            # Display the uploaded image
+            # Display the uploaded image with a width of 800 pixels
             st.image(uploaded_file, caption="Uploaded Wardley Map", width=800)
 
             # Make an API request to the /v2/analyse_map endpoint with the file
             files = {'file': uploaded_file}
             bearer_token = st.secrets["BEARER_TOKEN"]
             headers = {'Authorization': f'Bearer {bearer_token}'}
-            with st.spinner("Analysing your Wardley Map ...."):
-                response = requests.post('https://api.wardleymaps.ai/v1/analyse-wardleymap-image', files=files, headers=headers)
+            response = requests.post('https://api.wardleymaps.ai/v1/analyse-wardleymap-image', files=files, headers=headers, timeout=240)
 
             if response.status_code == 200:
                 return response.json()
             else:
                 st.error(f"Error: {response.status_code}, {response.text}")
                 return None
+    except requests.Timeout:
+        st.error("The request timed out. Please try again later.")
+        return None
     except Exception as e:
         st.error(f"Error uploading file: {e}")
         return None
